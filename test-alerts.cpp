b4150891281e22282055eae86405e5a8269ab633
Gopinath <25692465+devgopinath@users.noreply.github.com>
2022-07-17 15:50:58 +0530

    added test cases for classify breach

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -10,3 +10,19 @@ TEST_CASE("infers the breach according to limits")
     REQUIRE(inferBreach(20, 20, 30) == NORMAL);
     REQUIRE(inferBreach(30, 20, 30) == NORMAL);
 }
+
+TEST_CASE("classify breach")
+{
+    REQUIRE(classifyBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+    REQUIRE(classifyBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
+
+    REQUIRE(classifyBreach(PASSIVE_COOLING, 0) == NORMAL);
+    REQUIRE(classifyBreach(PASSIVE_COOLING, 20) == NORMAL);
+    REQUIRE(classifyBreach(PASSIVE_COOLING, 35) == NORMAL);
+    REQUIRE(classifyBreach(MED_ACTIVE_COOLING, 0) == NORMAL);
+    REQUIRE(classifyBreach(MED_ACTIVE_COOLING, 20) == NORMAL);
+    REQUIRE(classifyBreach(MED_ACTIVE_COOLING, 40) == NORMAL);
+
+    REQUIRE(classifyBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
+    REQUIRE(classifyBreach(MED_ACTIVE_COOLING, 41) == TOO_HIGH);
+}
